74873b00ac8e7a5c1dd5dd30efc73b0f91b09537
oyfhmi83 <66204886+oyfhmi83@users.noreply.github.com>
2020-06-03 10:22:29 +0300

    Update Uygulama5.java

--- Final uygulama 2/uygulama5/src/uygulama5/Uygulama5.java
+++ Final uygulama 2/uygulama5/src/uygulama5/Uygulama5.java
@@ -1,13 +1,13 @@
 ﻿Parametresiz Metotlar / Uygulama 5
 
 package uygulama5;
-import java.util.Scanner; 
+import java.util.Scanner; //Kullanıcının girdiği değerleri okuyabilmek için util dahil ediyorum
 public class Uygulama5 {
     public static void main(String[] args) {
 
 String[] gunler={ "Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar" };
-Scanner giris=new Scanner(System.in);  
-System.out.println("Hafta'nın gününü öğrenmek için bir sayı giriniz.. 1 ile 7 arasında"); 
+Scanner giris=new Scanner(System.in);   //Kullanıcının girdiği değerleri okuyabilmek için util dahil ediyorum
+System.out.println("Hafta'nın gününü öğrenmek için bir sayı giriniz.. 1 ile 7 arasında"); //Günler değişkenini ekrana yazdırdım.
 int sayiGir=giris.nextInt();
 
 for (int i = 1; i < gunler.length+1; i++)  
