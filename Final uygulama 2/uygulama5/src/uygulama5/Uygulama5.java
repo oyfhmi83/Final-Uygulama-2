32af128d8fe67bbfdb36cd2b51bb4d4ecae5a804
oyfhmi83 <66204886+oyfhmi83@users.noreply.github.com>
2020-06-03 10:20:33 +0300

    Add files via upload

---
+++ Final uygulama 2/uygulama5/src/uygulama5/Uygulama5.java
@@ -0,0 +1,21 @@
+﻿Parametresiz Metotlar / Uygulama 5
+
+package uygulama5;
+import java.util.Scanner; 
+public class Uygulama5 {
+    public static void main(String[] args) {
+
+String[] gunler={ "Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi","Pazar" };
+Scanner giris=new Scanner(System.in);  
+System.out.println("Hafta'nın gününü öğrenmek için bir sayı giriniz.. 1 ile 7 arasında"); 
+int sayiGir=giris.nextInt();
+
+for (int i = 1; i < gunler.length+1; i++)  
+{
+  if(sayiGir==i) //
+  {
+    System.out.println(gunler[i-1]);
+  }
+          
+}
+}
